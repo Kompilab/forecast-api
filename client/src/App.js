952177de04c5571046f1b6d72c1cd624e08bde84
ceemion <fiyinsimeon@gmail.com>
2018-10-15 14:31:51 +0100

    Add react router, wire up with 404 page :

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     );
   }
 }
