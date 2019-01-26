dbdb7a6d6452ac59ff970a35034ba185a15cde06
ceemion <fiyinsimeon@gmail.com>
2019-01-26 15:37:02 +0100

    Use boostrap navbar to style homepage nav

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -17,30 +17,42 @@ class Navbar extends Component {
     const { title, history } = this.props;
 
     return (
-      <div>
-        <div>{title || titleOverride}</div>
-        <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" id="logo-title" className="navbar-brand">
+          {title || titleOverride}
+        </Link>
+
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav mr-auto"></div>
+          <div>
           {
             userAuth.isAuthenticated ? (
               <div>
-                <p>Welcome home!</p>
-                <button onClick={() => {userAuth.signOut(() => history.push('/'))}}>
+                <span className="navbar-text">
+                  Welcome home!
+                </span>
+                <button onClick={() => {userAuth.signOut(() => history.push('/'))}} className="btn btn-outline-danger my-2 my-sm-0 ml-2">
                   Sign Out
                 </button>
               </div>
             ) : (
               <div>
                 <Link to="/auth/login">
-                  <button>Log In</button>
+                  <button className="btn btn-light my-2 my-sm-0">Log In</button>
                 </Link>
                 <Link to="/auth/sign-up">
-                  <button>Sign Up</button>
+                  <button className="btn btn-primary btn-fo-primary my-2 my-sm-0 ml-2">Sign Up</button>
                 </Link>
               </div>
             )
           }
+          </div>
         </div>
-      </div>
+      </nav>
     )
   }
 }
