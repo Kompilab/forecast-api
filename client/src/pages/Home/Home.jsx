b3436d595a96283d71c7d4c592c2218477aacf45
ceemion <fiyinsimeon@gmail.com>
2019-01-28 13:19:50 +0100

    Style header

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -29,21 +29,29 @@ class Home extends Component {
       <div>
         <Navbar title={logoText} {...this.props} />
 
-        <header className="container-fluid">
-          <div className="row">
-            <div className="col-sm">
-              <p>How much have I spend this month?</p>
+        <header className="container-fluid header">
+          <div className="row align-items-center">
+            <div className="col-sm left-text">
+              <div className="row">
+                <div className="col-md-6 offset-md-3">
+                  <p>How much have I spent this month?</p>
+                </div>
+              </div>
             </div>
 
-            <div className="col-sm">
-              <p>"The pessimist complains about the wind; the optimist expects it to change; the realist adjusts the sails."</p>
-              <p>- William Arthur Ward</p>
+            <div className="col-sm right-text">
+              <div className="row">
+                <div className="col-md-6 offset-md-3 text-center align-self-center">
+                  <p>"The pessimist complains about the wind; the optimist expects it to change; the realist adjusts the sails."</p>
+                  <p>- William Arthur Ward</p>
+                </div>
+              </div>
             </div>
           </div>
 
           <div className="row justify-content-center">
             <Link to="/dashboard">
-              <button className="btn btn-primary btn-fo-primary my-2 my-sm-0">Get Started</button>
+              <button className="btn btn-primary btn-fo-primary my-2 my-sm-0 btn-lg">Get Started</button>
             </Link>
           </div>
         </header>
