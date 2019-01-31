0ceaaa595008b78d198a3ecba5c4eac2c0486fdf
ceemion <fiyinsimeon@gmail.com>
2019-01-31 12:18:47 +0100

    Add react-web-vector-icons for vector icons

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-web-vector-icons/fonts';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
