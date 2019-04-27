63959aa5bcebc48e815e0e91b18dc3c1be415402
ceemion <fiyinsimeonn@gmail.com>
2019-04-27 18:01:30 +0100

    [testing] deploy public folder for heroku testing

---
+++ public/precache-manifest.f4b5ab2245b9d52e09098055b64188c0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "148a7b8318eda15b63b3",
+    "url": "/static/css/main.aaa242a8.chunk.css"
+  },
+  {
+    "revision": "148a7b8318eda15b63b3",
+    "url": "/static/js/main.148a7b83.chunk.js"
+  },
+  {
+    "revision": "c448aed33c7b677283f9",
+    "url": "/static/css/1.3bee6ef2.chunk.css"
+  },
+  {
+    "revision": "c448aed33c7b677283f9",
+    "url": "/static/js/1.c448aed3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/static/media/AntDesign.3a2ba315.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/static/media/Feather.6beba7e6.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/static/media/Foundation.e20945d7.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "d15c1216957060fac577af6151fb8cfe",
+    "url": "/static/media/MaterialCommunityIcons.d15c1216.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "73b8cff012825060b308d2162f31dbb2",
+    "url": "/static/media/Octicons.73b8cff0.ttf"
+  },
+  {
+    "revision": "5cdf883b18a5651a29a4d1ef276d2457",
+    "url": "/static/media/Zocial.5cdf883b.ttf"
+  },
+  {
+    "revision": "06248f3342288d7681fc55e2ffd7cd17",
+    "url": "/static/media/AvenirNextLTPro-It.06248f33.otf"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "210aec67005734b028baebf9e8b2be8a",
+    "url": "/static/media/Italianno-Regular.210aec67.ttf"
+  },
+  {
+    "revision": "c41263ef0000160e357a6b8afbff345b",
+    "url": "/static/media/Rajdhani-Regular.c41263ef.ttf"
+  },
+  {
+    "revision": "3800a73b911b8c7753d9d91b49548b80",
+    "url": "/static/media/floating-img.3800a73b.png"
+  },
+  {
+    "revision": "2963c935de630f13ed68ed674e1c9bf1",
+    "url": "/static/media/header-bg.2963c935.jpg"
+  },
+  {
+    "revision": "f9069a22fc93e04bde6e55880cf633b5",
+    "url": "/index.html"
+  }
+];
