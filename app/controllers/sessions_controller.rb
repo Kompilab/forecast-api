04eba43ed335c30ac5887203e943c1dbbeefa142
ceemion <fiyinsimeonn@gmail.com>
2019-05-02 01:36:02 +0100

    Update sessions create to properly save event on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,18 @@ class SessionsController < Devise::SessionsController
   respond_to :json
 
   def create
-    super
+    self.resource = warden.authenticate!(auth_options)
+
     Events::Logger.new(
-      event_name: 'auth.login',
-      description: "You logged in",
-      event_date: Date.today,
-      event_type: 'login',
-      user_id: resource.id
+        event_name: 'auth.login',
+        description: "You logged in",
+        event_date: Date.today,
+        event_type: 'login',
+        user_id: resource.id
     ).log
+
+    sign_in(resource_name, resource)
+    render_resource(resource)
   end
 
   private
